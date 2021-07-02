5e29e23c973183d572a45b53faf44c13291dd4c6
hank <hankchiu.tw@gmail.com>
2021-07-02 21:25:50 +0800

    init

---
+++ src/entities.js
@@ -0,0 +1,3 @@
+define({
+  User: class User {},
+});
---
+++ src/main.js
@@ -0,0 +1,4 @@
+require(['src/entities', 'dojo/dom'], function({ User }, dom) {
+  const user = new User();
+  console.log('xxx: User', user, dom);
+});
